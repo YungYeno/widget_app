281662de9eb02eb98e385789328e372af8f097a7
Yeno <yenoreus@hotmail.com>
2020-10-09 23:35:21 +0200

    Widget app
    push iets

---
+++ widget_wrap/lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+        title: "Wrap widget",
+        theme: new ThemeData(
+            primarySwatch: Colors.green
+        ),
+        debugShowCheckedModeBanner: false,
+        home: WrapWidget()
+    );
+  }
+}
+
+class WrapWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:AppBar(
+        title: Text("Wrap Widget opdracht"),
+      ),
+      body: Wrap(
+            direction: Axis.vertical,
+            spacing:8.0,
+        children: <Widget>[
+          Container(
+              color: Colors.blue,
+              width: 100,
+              height: 100,
+              child:Center(child: Text("Een wrap",textScaleFactor: 2.5,))
+          ),
+          Container(
+              color: Colors.red,
+              width: 100,
+              height: 100,
+              child:Center(child: Text("Een wrap",textScaleFactor: 2.5,))
+          ),
+          Container(
+              color: Colors.teal,
+              width: 100,
+              height: 100,
+              child:Center(child: Text("Een wrap",textScaleFactor: 2.5,))
+          ),
+          Container(
+              color: Colors.indigo,
+              width: 100,
+              height: 100,
+              child:Center(child: Text("Een wrap",textScaleFactor: 2.5,))
+          ),
+          Container(
+              color: Colors.orange,
+              width: 100,
+              height: 100,
+              child:Center(child: Text("Een wrap",textScaleFactor: 2.5,))
+          ),
+        ],
+      ),
+    );
+  }
+}
