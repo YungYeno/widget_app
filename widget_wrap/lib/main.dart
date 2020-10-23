680a742d35c5c55aa10d7189cce8382f285c3203
Marco Bruijns <marcobruijns@live.nl>
2020-10-23 23:41:54 +0200

    Added

--- widget_wrap/lib/main.dart
+++ widget_wrap/lib/main.dart
@@ -18,46 +18,96 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Second Route"),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
+
+
 class WrapWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
+      appBar: AppBar(
         title: Text("Wrap Widget opdracht"),
       ),
       body: Wrap(
-            direction: Axis.vertical,
-            spacing:8.0,
+        direction: Axis.vertical,
+        spacing: 8.0,
         children: <Widget>[
           Container(
-              color: Colors.blue,
-              width: 100,
-              height: 100,
-              child:Center(child: Text("Een wrap",textScaleFactor: 2.5,))
+            color: Colors.blue,
+            width: 100,
+            height: 100,
+            child: Center(child: Text("Een wrap", textScaleFactor: 2.5,)),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SecondRoute()),
+              );
+            },
           ),
           Container(
-              color: Colors.red,
-              width: 100,
-              height: 100,
-              child:Center(child: Text("Een wrap",textScaleFactor: 2.5,))
+            color: Colors.red,
+            width: 100,
+            height: 100,
+            child: Center(child: Text("Een wrap", textScaleFactor: 2.5,)),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SecondRoute()),
+              );
+            },
           ),
           Container(
-              color: Colors.teal,
-              width: 100,
-              height: 100,
-              child:Center(child: Text("Een wrap",textScaleFactor: 2.5,))
+            color: Colors.teal,
+            width: 100,
+            height: 100,
+            child: Center(child: Text("Een wrap", textScaleFactor: 2.5,)),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SecondRoute()),
+              );
+            },
           ),
           Container(
-              color: Colors.indigo,
-              width: 100,
-              height: 100,
-              child:Center(child: Text("Een wrap",textScaleFactor: 2.5,))
+            color: Colors.indigo,
+            width: 100,
+            height: 100,
+            child: Center(child: Text("Een wrap", textScaleFactor: 2.5,)),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SecondRoute()),
+              );
+            },
           ),
           Container(
-              color: Colors.orange,
-              width: 100,
-              height: 100,
-              child:Center(child: Text("Een wrap",textScaleFactor: 2.5,))
+            color: Colors.orange,
+            width: 100,
+            height: 100,
+            child: Center(child: Text("Een wrap", textScaleFactor: 2.5,)),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SecondRoute()),
+              );
+            },
           ),
         ],
       ),
